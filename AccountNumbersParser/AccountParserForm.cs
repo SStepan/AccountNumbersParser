1c6e875d5a12272be68ff514523c9453d7f58ae9
Stepan Myroniuk <myronyuk.s@gmail.com>
2018-02-26 10:27:52 +0200

    first commit

---
+++ AccountNumbersParser/AccountParserForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace AccountNumbersParser
+{
+    public partial class AccountParserForm : Form
+    {
+        private string filePath;
+        public AccountParserForm()
+        {
+            InitializeComponent();
+            textBox1.ReadOnly = true;
+            openFileDialog1.InitialDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if(result == DialogResult.OK) {
+                filePath = openFileDialog1.FileName;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(filePath)) {
+                StreamReader streamReader = new StreamReader(filePath);
+
+                var text = streamReader.ReadToEnd();
+
+                Regex regex = new Regex(@"[^\s_|]");
+                if (regex.IsMatch(text)) {
+                    MessageBox.Show("File is not valid. Choose another file.");
+                    button2.Enabled = false;
+                    return;
+                }
+
+                List<string> faxNumbers = new List<string>();
+                string[] lines = text.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                if (lines.Length % 3 != 0) {
+                    MessageBox.Show("File is not valid. Choose another file.");
+                    button2.Enabled = false;
+                    return;
+                }
+                else
+                {
+                    for (int i = 0; i < lines.Length; i = i + 3) 
+                    {
+                        string number = string.Empty;
+                        for (int j = 0; j < 9; j++) {
+                            var faxNumber = string.Empty;
+                            int skipCount = 0;
+                            if (j != 0) {
+                                skipCount = j * 3;
+                            }
+
+                            faxNumber = lines[i].Substring(skipCount, 3);
+                            faxNumber += lines[i + 1].Substring(skipCount, 3);
+                            faxNumber += lines[i + 2].Substring(skipCount, 3);
+                            number += FaxNumbers.GetNumber(faxNumber);
+                        }
+                        faxNumbers.Add(number);
+                    }
+                }
+
+                textBox1.Text = String.Join("\r\n", faxNumbers);
+            }
+        }
+    }
+}
---
+++ AccountNumbersParser/FaxNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountNumbersParser
+{
+    public class FaxNumbers
+    {
+        public const string Zero = " _ | ||_|"; 
+        public const string One = "     |  |";
+        public const string Two = " _  _||_ ";
+        public const string Three = " _  _| _|";
+        public const string Four = "   |_|  |";
+        public const string Five = " _ |_  _|";
+        public const string Six = " _ |_ |_|";
+        public const string Seven = " _   |  |";
+        public const string Eight = " _ |_||_|";
+        public const string Nine = " _ |_| _|";
+
+        public static string GetNumber(string faxNumber)
+        {
+            switch (faxNumber) 
+            {
+                case Zero:
+                    return "0";
+                case One:
+                    return "1";
+                case Two:
+                    return "2";
+                case Three:
+                    return "3";
+                case Four:
+                    return "4";
+                case Five:
+                    return "5";
+                case Six:
+                    return "6";
+                case Seven:
+                    return "7";
+                case Eight:
+                    return "8";
+                case Nine:
+                    return "9";
+                default:
+                    return "";
+            }
+        }
+    }
+}
